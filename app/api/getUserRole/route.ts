74bed0f041703eb6628a35d8cf12038852a1de28
Serigne Mbaye Sy Amar <111590312+MbayeSyAmar@users.noreply.github.com>
2025-10-15 21:28:53 +0200

    deploiement

--- app/api/getUserRole/route.ts
+++ app/api/getUserRole/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase"; // ton fichier est déjà prêt
 import { doc, getDoc } from "firebase/firestore";
+export const dynamic = "force-dynamic"; // <-- Important pour Firebase
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
