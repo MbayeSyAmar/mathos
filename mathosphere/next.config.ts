d65b8ed1d6e323bf6e833a241d5632ce2ec7d4f5
Amar233-ui <mamegoumbaamar69@gmail.com>
2025-10-15 17:36:14 +0000

    JE deploie

--- mathosphere/next.config.ts
+++ mathosphere/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/mathos', // Remplace par le nom de ton repo GitHub
+  assetPrefix: '/mathos/',
 };
 
 export default nextConfig;
